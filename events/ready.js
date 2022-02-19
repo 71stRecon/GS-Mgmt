d1e85ac3ba502e624cc390c6266267dca642ba82
Chase <c@chse.dev>
2022-02-18 20:10:05 -0600

    missing mg check

--- events/ready.js
+++ events/ready.js
@@ -43,6 +43,11 @@ export default {
             console.error(`Please add this bot to a server before starting it!\nhttps://discord.com/api/oauth2/authorize?client_id=${ client.user.id }&permissions=8&scope=bot%20applications.commands`);
             process.exit(1);
         }
+        if (client.guilds.cache.size > 1)
+        {
+            console.error(`ERROR: This bot does not support multi-guild.\nPlease remove it from every guild except one.`);
+            process.exit(1);
+        }
 
         console.log(`Bot is ready!`);
     },
